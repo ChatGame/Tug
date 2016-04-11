b0ec4cb9482f8f0089a5babd8fd17e467145e2ce
wang xing <xingwang@v5.cn>
2016-04-11 16:29:59 +0800

    set log message as empty string when it's null

--- tugcore/src/main/java/me/chatgame/mobilecg/tug/util/LogUtil.java
+++ tugcore/src/main/java/me/chatgame/mobilecg/tug/util/LogUtil.java
@@ -11,26 +11,41 @@ public class LogUtil {
     public static int LOG_LEVEL = Log.INFO;
     public static void logV(String message, Object... args) {
         if (NEED_LOG && LOG_LEVEL <= Log.VERBOSE) {
+            if (message == null) {
+                message = "";
+            }
             Log.v(TAG, String.format(message, args));
         }
     }
     public static void logD(String message, Object... args) {
         if (NEED_LOG && LOG_LEVEL <= Log.DEBUG) {
+            if (message == null) {
+                message = "";
+            }
             Log.d(TAG, String.format(message, args));
         }
     }
     public static void logI(String message, Object... args) {
         if (NEED_LOG && LOG_LEVEL <= Log.INFO) {
+            if (message == null) {
+                message = "";
+            }
             Log.i(TAG, String.format(message, args));
         }
     }
     public static void logW(String message, Object... args) {
         if (NEED_LOG && LOG_LEVEL <= Log.WARN) {
+            if (message == null) {
+                message = "";
+            }
             Log.w(TAG, String.format(message, args));
         }
     }
     public static void logE(String message, Object... args) {
         if (NEED_LOG && LOG_LEVEL <= Log.ERROR) {
+            if (message == null) {
+                message = "";
+            }
             Log.e(TAG, String.format(message, args));
         }
     }
